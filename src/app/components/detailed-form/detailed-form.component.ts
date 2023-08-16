c218b8ae244d13297869431c4b65798c883b5dab
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-08-16 15:51:56 -0500

    detailed form component created with DI

--- src/app/components/detailed-form/detailed-form.component.ts
+++ src/app/components/detailed-form/detailed-form.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { FormService } from 'src/app/services/form/form.service';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { DetailedFormService } from 'src/app/services/detailed-form/detailed-form.service';
 
 @Component({
   selector: 'app-detailed-form',
@@ -12,8 +14,16 @@ import { FormService } from 'src/app/services/form/form.service';
 })
 export class DetailedFormComponent {
 
-  form = this.formService.getForm();
+  // form = this.formService.getForm();
 
-  constructor(private formService: FormService) { }
+  constructor(
+    private detailedFormService: DetailedFormService
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+  // TODO: check permissions, if equal to 'denied' or 'prompt' then ask for permissions; if equal to 'granted' then proceed to get location; if error then ask to enable location in an window alert
 
 }
