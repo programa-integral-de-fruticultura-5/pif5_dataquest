d872c5be2b992c46009d9093309dc0de89908cd3
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-09-01 16:17:18 -0500

    child questions added to table type questions

--- src/app/services/form/form.service.ts
+++ src/app/services/form/form.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from '../api/api.service';
 import { Form } from 'src/app/models/form';
 import { HttpResponse } from '@capacitor/core';
+import { Question } from 'src/app/models/question';
 
 const ENDPOINT = 'form-detail';
 
@@ -22,13 +23,46 @@ export class FormService {
     var response: Promise<HttpResponse> = this.apiService.post(ENDPOINT);
 
     response.then(
-      (forms) => {
-        this.forms = JSON.parse(forms.data);
+      (formsResponse) => {
+        const forms = JSON.parse(formsResponse.data);
+
+        // Iterate through each form and initialize questionChildren arrays
+        forms.forEach((form: Form) => {
+          form.questions.forEach((question: Question) => {
+            if (question.type === 'Tabla') {
+              let children: Question[][] = this.getQuestionChildren(question, form);
+              question.questionChildren = children;
+            }
+          });
+        });
+
+        this.forms = forms;
+        console.log(this.forms)
       },
       (err) => {
         console.log(err);
       }
     );
+  }
+
+  private getQuestionChildren(question: Question, form: Form): Question[][] {
+    let children: Question[][] = [];
+    let base: Question[] = [];
+
+    // Iterate through each question of the form
+    form.questions.forEach((q: Question) => {
+      // If the question is a child of the current question
+      if (q.questionParentId === question.id) {
+        // Add the question to the base array
+        base.push(q);
+      }
+    });
+
+    // Add the base array to the children array
+    children.push(base);
+
+    return children;
+
   }
 
   public getForms(): Form[] {
