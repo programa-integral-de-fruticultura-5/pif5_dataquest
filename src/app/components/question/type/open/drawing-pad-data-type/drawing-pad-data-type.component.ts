1edc28a4ca2849027c87b0b1246b4b91e58764e0
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-09-05 14:53:14 -0500

    feat: drawing pad component and dependencies

--- src/app/components/question/type/open/drawing-pad-data-type/drawing-pad-data-type.component.ts
+++ src/app/components/question/type/open/drawing-pad-data-type/drawing-pad-data-type.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import SignaturePad from 'signature_pad';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-drawing-pad-data-type',
   templateUrl: './drawing-pad-data-type.component.html',
   styleUrls: ['./drawing-pad-data-type.component.scss'],
   standalone: true,
+  imports: [ IonicModule ]
 })
-export class DrawingPadDataTypeComponent  implements OnInit {
+export class DrawingPadDataTypeComponent {
+
+  private signaturePad!: SignaturePad;
+
+  @ViewChild('canvas')
+  canvas!: ElementRef;
+
+  signatureImg!: string;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngAfterViewInit() {
+    this.signaturePad = new SignaturePad(this.canvas.nativeElement);
+    this.savePad();
+    this.signaturePad.throttle = 0;
+    this.signaturePad.minDistance = 0;
+    this.canvas.nativeElement.style.width = '100%';
+    this.canvas.nativeElement.style.height = '100%';
+    this.canvas.nativeElement.width = this.canvas.nativeElement.offsetWidth;
+    this.canvas.nativeElement.height = this.canvas.nativeElement.offsetHeight;
+  }
 
+  startDrawing(event: Event) {
+    console.log(event);
+    // works in device not in browser
+
+  }
+
+  moved(event: Event) {
+    // works in device not in browser
+  }
+
+  clearPad() {
+    this.signaturePad.clear();
+  }
+
+  savePad() {
+    const base64Data = this.signaturePad.toDataURL();
+    this.signatureImg = base64Data;
+  }
+
+  undoPad() {
+    const data = this.signaturePad.toData();
+    if (data) {
+      data.pop();
+      this.signaturePad.fromData(data);
+    }
+  }
 }
