4bc64fb39450292a55dd90509605d391a6de67b7
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-09-14 14:30:11 -0500

    feat: date-fns library implemented for date parsing

--- src/app/components/question/type/open/date-data-type/date-data-type.component.ts
+++ src/app/components/question/type/open/date-data-type/date-data-type.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Question } from 'src/app/models/question';
+import { format, formatISO, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-date-data-type',
@@ -17,6 +18,32 @@ export class DateDataTypeComponent  implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  getValue(): string {
+    let value: string = this.formGroup.get(`${this.question.id}`)?.value;
+    let date: Date = value ? parseISO(value) : new Date();
+    return formatISO(date)
+  }
+
+  setValue(event: any): void {
+    this.formGroup.get(`${this.question.id}`)?.setValue(event.detail.value);
+  }
+
+  getMinToMaxDate(): string {
+    let maxDate: string = this.question.min ? formatISO(new Date()) : '';
+    return maxDate;
+  }
+
+  getMaxToMinDate(): string {
+    let minDate: string = this.question.max ? this.substractYears(this.question.max) : '';
+    return minDate;
+  }
+
+  substractYears(years: number): string {
+    let date: Date = new Date();
+    date.setFullYear(date.getFullYear() + years);
+    return formatISO(date);
+  }
 
 }
