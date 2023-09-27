829de2cbf3caedf54df896e843d03c96148d5a35
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-09-27 09:34:16 -0500

    feat: getLocation() implemented when started

--- src/app/components/detailed-form/detailed-form.component.ts
+++ src/app/components/detailed-form/detailed-form.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { AlertController, IonicModule } from '@ionic/angular';
+import { AlertController, IonicModule, NavController } from '@ionic/angular';
 import { FormService } from 'src/app/services/form/form.service';
 import { Geolocation, Position } from '@capacitor/geolocation';
 import { DetailedFormService } from 'src/app/services/detailed-form/detailed-form.service';
@@ -13,67 +13,84 @@ import { DataquestHeaderComponent } from '../header/dataquest-header/dataquest-h
   templateUrl: './detailed-form.component.html',
   styleUrls: ['./detailed-form.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule, QuestionComponent, DataquestHeaderComponent ],
+  imports: [
+    CommonModule,
+    IonicModule,
+    QuestionComponent,
+    DataquestHeaderComponent,
+  ],
 })
 export class DetailedFormComponent {
-
-  component = QuestionComponent
+  component = QuestionComponent;
 
   constructor(
     private detailedFormService: DetailedFormService,
     private formService: FormService,
-    private alertController: AlertController
-  ) { }
+    private alertController: AlertController,
+    private navController: NavController
+  ) {}
 
   getForm(): Form {
     return this.detailedFormService.getForm();
   }
 
-  // TODO move location logic to service
-
-/*   async checkPermissions(): Promise<void> {
-    const locationAlert = await this.alertController.create({
-      header: 'Problema con la ubicación',
-      message: 'No se pudo obtener la ubicación del dispositivo. Por favor, habilita la ubicación en los ajustes del dispositivo y vuelve a intentarlo.',
-      buttons: ['OK'],
-    });
-
-    Geolocation.checkPermissions().then((result) => {
-      if(result.location !== 'granted') {
-        this.requestPermissions(locationAlert)
-      }
-      this.getLocation();
-    }).catch((err) => {
-      locationAlert.present();
-    })
+  startForm(): void {
+    this.getLocation();
   }
 
-  private async requestPermissions(locationAlert: HTMLIonAlertElement): Promise<void> {
-    const permissionAlert = await this.alertController.create({
-      header: 'Problema con los permisos',
-      message: 'No se pudo obtener los permisos de ubicación del dispositivo. Por favor, habilita los permisos de ubicación en los ajustes del dispositivo y vuelve a intentarlo.',
-      buttons: ['OK'],
-    });
+  private async getLocation(): Promise<void> {
+    try {
+      const permissions = await Geolocation.checkPermissions();
 
-    Geolocation.requestPermissions().then(async (result) => {
-      if(result.location !== 'granted') {
-        permissionAlert.present();
+      if (permissions.location !== 'granted') {
+        const permissionAlert = await this.alertController.create({
+          header: 'Problema con los permisos',
+          message:
+            'No se pudo obtener los permisos de ubicación del dispositivo. Por favor, habilita los permisos de ubicación en los ajustes del dispositivo y vuelve a intentarlo.',
+          buttons: ['OK'],
+        });
+
+        const permissionResult = await Geolocation.requestPermissions();
+
+        if (permissionResult.location !== 'granted') {
+          permissionAlert.present();
+          return;
+        }
       }
-      this.getLocation();
-    },
-    async (err) => {
-      permissionAlert.present();
+      const locationAlert = await this.alertController.create({
+        header: 'Problema con la ubicación',
+        message:
+          'No se pudo obtener la ubicación del dispositivo. Por favor, habilita la ubicación en los ajustes del dispositivo y vuelve a intentarlo.',
+        buttons: ['OK'],
+      });
+
+      const position = await Geolocation.getCurrentPosition();
+
+      if (position) {
+        console.log(position.coords)
+        this.getForm().position =
+          position.coords.latitude + ',' + position.coords.longitude;
+        this.getForm().altitud = position.coords.altitude ?? 0;
+        console.log(
+          'Position: ',
+          position,
+          ', Altitude: ',
+          this.getForm().altitud
+        );
+      } else {
+        locationAlert.present();
+      }
+    } catch (error) {
+      const locationAlert = await this.alertController.create({
+        header: 'Servicios de ubicación deshabilitados',
+        message:
+          'No se pudo obtener la ubicación del dispositivo. Por favor, habilita la ubicación en los ajustes del dispositivo y vuelve a intentarlo.',
+        buttons: ['OK'],
+      });
+      locationAlert.present();
+      this.navController.pop();
     }
-    ).catch((err) => {
-
-    })
-  } */
-
- /*  getLocation(): void {
-    Geolocation.getCurrentPosition().then((position) => {
-      this.form.position = position.coords.latitude + ',' + position.coords.longitude;
-    })
-  } */
+  }
 
   getTotalQuestions(): number {
     return this.detailedFormService.getTotalQuestions();
