aa19b7e8505ca8a83a2eef77d4623d7311d810ee
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-09-27 09:35:19 -0500

    feat: error handling when there is no internet

--- src/app/services/survey/survey.service.ts
+++ src/app/services/survey/survey.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Form } from 'src/app/models/form';
 import { StorageService } from '../storage/storage.service';
 import { ApiService } from '../api/api.service';
+import { Network } from '@capacitor/network';
+import { AlertController } from '@ionic/angular';
 
 const ENDPOINT = 'uploadSurveys';
 
@@ -13,6 +15,7 @@ export class SurveyService {
 
   constructor(
     private apiService: ApiService,
+    private alertController: AlertController,
     private storageService: StorageService
   ) {
     this.surveys = [];
@@ -40,7 +43,39 @@ export class SurveyService {
     this.storageService.set('surveys', this.surveys);
   }
 
-  syncSurveys() {
-    this.apiService.post(ENDPOINT, this.surveys)
+  async syncSurveys() {
+    const { connected } = await Network.getStatus();
+    if (connected) {
+      this.surveys.forEach((survey) => {
+        this.apiService.post(ENDPOINT, survey).then((res) => {
+          if (res.status === 200) {
+            this.changeSyncStatus(survey, true);
+          }
+        }).catch(async (err) => {
+          const serverAlert = await this.alertController.create({
+            header: 'Problema con el servidor',
+            subHeader: 'Error ' + err.status,
+            message:
+              'No se pudo establecer una conexión con el servidor. Por favor, intenta más tarde.',
+            buttons: ['OK'],
+          })
+          serverAlert.present();
+        });
+      })
+      this.saveSurveys()
+    } else {
+      const noInternetAlert = await this.alertController.create({
+        header: 'No hay conexión a internet',
+        message:
+          'No se pudo establecer una conexión a internet. Por favor, verifica tu conexión a internet y vuelve a intentarlo.',
+        buttons: ['OK'],
+      });
+      noInternetAlert.present();
+    }
+  }
+
+  private changeSyncStatus(survey: Form, status: boolean): void {
+    const index = this.surveys.indexOf(survey);
+    this.surveys[index].sync = status;
   }
 }
