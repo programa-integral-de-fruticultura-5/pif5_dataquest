81a8983a0bf8e122c62bba66f618089be3380e0b
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-10-13 08:50:34 -0500

    app bundle id modified

--- android/app/src/main/java/com/blaucast/dataquest/app/MainActivity.java
+++ android/app/src/main/java/com/blaucast/dataquest/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.blaucast.dataquest.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.blaucast.dataquest.app',
   appName: 'Dataquest',
   webDir: 'www',
   server: {
