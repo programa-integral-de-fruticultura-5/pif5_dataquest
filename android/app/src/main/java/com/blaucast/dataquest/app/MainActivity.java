8203ad87d5d743b2bffdbad47ea45dade295154b
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-10-31 13:26:33 -0500

    android folder regenerated and ios removed

--- android/app/src/main/java/com/blaucast/dataquest/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.blaucast.dataquest.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ android/app/src/main/java/com/blaucastmedia/dataquest/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.blaucastmedia.dataquest.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
