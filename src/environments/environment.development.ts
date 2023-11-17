8957a97868b8efa9298e830d861173505ec22060
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-11-17 00:19:19 -0500

    add: endpoints

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,9 @@ export const environment = {
   production: false,
   url: 'http://localhost:3000',
   appVersion: '1.0.0',
+  authEndpoint: 'auth/login',
+  formsEndpoint: 'form-detail',
+  producersEndpoint: 'producers',
+  associationsEndpoint: 'associations',
+  syncEndpoint: 'uploadSurvey',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,9 @@ export const environment = {
   production: true,
   url: 'https://pif5.blaucast.com',
   appVersion: '1.0.0',
+  authEndpoint: 'auth/login',
+  formsEndpoint: 'form-detail',
+  producersEndpoint: 'producers',
+  associationsEndpoint: 'associations',
+  syncEndpoint: 'uploadSurvey',
 };
