bf12c485a416a75da2da95c144aa1fd2c1f76b01
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-11-17 00:18:24 -0500

    change: beneficiary folder to beneficiary namespace

---
+++ src/app/models/Beneficiary.namespace.ts
@@ -0,0 +1,50 @@
+export namespace Beneficiary {
+
+  export interface Producer {
+    cedula: string;
+    firstname: string;
+    middlename: string;
+    lastname: string;
+    secondLastname: string;
+    id: string;
+    specialized: boolean;
+    technicalAssistance: boolean;
+    demonstrationPlot: boolean;
+    greenhouse: boolean;
+    supplies: boolean;
+    associationId: number;
+  }
+
+  export type ProducerResponse = {
+    cedula: string;
+    primer_nombre: string;
+    segundo_nombre: string;
+    primer_apellido: string;
+    segundo_apellido: string;
+    identification: string;
+    has_especializada: number;
+    at_p5: number;          // asistencia técnica
+    pd_p5: number;          // parcela demostrativa
+    cm_p5: number;          // casa malla (invernadero espacial)
+    insumo_p5: number;      // insumos
+    association_id: number; // id de la asociacion
+  }
+
+  export interface Association {
+    id: number;
+    nit: number;
+    name: string;
+    identification: string;
+    zone: string;
+    farming: string;
+  }
+
+  export type AssociationResponse = {
+    id: number;
+    nit: number;
+    name: string;
+    identification: string;
+    zona: string;
+    farming: string;
+  }
+}
