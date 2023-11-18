34d9faaa14c13306fc39bbceb2b0ae83e51da50a
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-11-18 04:46:33 -0500

    change: app version

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   url: 'https://pif5.blaucast.com',
-  appVersion: '1.0.0',
+  appVersion: '1.1.1',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: 'https://programaintegraldefruticultura.com.co',
-  appVersion: '1.0.0',
+  appVersion: '1.1.1',
 };
