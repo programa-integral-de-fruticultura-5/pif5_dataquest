185961d58ae33f437de1f83b957839986865bc39
Alexander Sanchez <alexandersanchez.jr@outlook.com>
2023-11-25 08:09:44 -0500

    update: api url for prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-/*   url: 'https://programaintegraldefruticultura.com.co', */
-  url: 'https://pif5.blaucast.com',
+  url: 'https://programaintegraldefruticultura.com.co',
   authEndpoint: 'auth/login',
   formsEndpoint: 'form-detail',
   producersEndpoint: 'producers',
