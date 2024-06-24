5fc4cc83def1d722949c98aa17c368147e3d8125
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-06-24 11:48:31 -0500

    add: script to extract app version

---
+++ extract-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Extract versionCode and versionName from build.gradle
+VERSION_CODE=$(grep -m1 versionCode android/app/build.gradle | awk '{print $2}')
+VERSION_NAME=$(grep -m1 versionName android/app/build.gradle | awk '{print $2}' | tr -d '"')
+
+echo "Version Code: $VERSION_CODE"
+echo "Version Name: $VERSION_NAME"
+
+# Set environment variables
+echo "VERSION_CODE=$VERSION_CODE" >> $GITHUB_ENV
+echo "VERSION_NAME=$VERSION_NAME" >> $GITHUB_ENV
